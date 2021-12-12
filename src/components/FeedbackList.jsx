2a0056445412cf3e929a8f8682838d165e47ad52
Aidan Byrne <aidanbyrne142@gmail.com>
2021-12-12 10:36:13 +0000

    Added Animations
    - Added animations to the Feedback List using the framer motion package library

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
@@ -9,13 +10,33 @@ function FeedbackList({ feedback, deleteItem }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem key={item.id} item={item} deleteItem={deleteItem} />
-        );
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem key={item.id} item={item} deleteItem={deleteItem} />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
+
+  // Return Without Animations
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => {
+  //       return (
+  //         <FeedbackItem key={item.id} item={item} deleteItem={deleteItem} />
+  //       );
+  //     })}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
